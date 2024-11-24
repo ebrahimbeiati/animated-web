93a76ec5821aecd7d403cb9e60ae59a0d3f4beef
Ebrahim Beiaty <ebrahimbeiaty@gmail.com>
2024-11-24 22:22:16 +0000

    message

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -27,14 +27,24 @@ const upcomingVideoIndex =(currentIndex % totalVideos) + 1;
                       <div onClick={handleMiniVdClick} className="origin-center scale-50 opacity-0 transition-all duration-300 ease-in hover:scale-100 hover:opacity-100">
                           <video autoPlay loop muted ref={nextVideoRef}src={getVideoSrc(upcomingVideoIndex)} id="current-video" className="size-64 origin-center scale-150 object-cover object-center" onLoadedData={handleVideoLoad}  />
                           
-                      </div>
-                 
-                    
+                      </div>   
                   </div>
                   <video
                           ref={nextVideoRef}
-                          src={getVideoSrc(currentIndex)}
-                      />
+                      src={getVideoSrc(currentIndex)}
+                      loop
+                      muted
+                      id="next-video"
+                      className="absolute-center invisible absolute z-20 size-64 object-cover object-center"
+                        onLoadedData={handleVideoLoad}
+                  />
+                  <video src={getVideoSrc(currentIndex === totalVideos -1 ? 1 : currentIndex)} autoPlay loop muted className="absolute left-0 top-0 size-full  object-cover object-center" onLoadedData={handleVideoLoad} />
+              </div>
+              <h1 className="special-font absolute  hero-heading bottom-5 right-5 z-40 text-blue-75">G<b>A</b>MING</h1>
+              <div className="absolute left-0 top-0 size-full">
+                  <div className="mt-24 px-5 sm">
+                      
+                  </div>
               </div>
               
           </div>
